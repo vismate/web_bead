3ebab12cc487c26c3a5dfb204c0c9dc3b5bb085c
VisMatt <visnyarmate@gmail.com>
2022-05-04 21:17:21 +0200

    yeah

---
+++ js/switchStylesheet.js
@@ -0,0 +1,9 @@
+function setStylesheet(title) {
+    var i, a;
+    for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+      if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+        a.disabled = true;
+        if(a.getAttribute("title") == title) a.disabled = false;
+      }
+    }
+  }
